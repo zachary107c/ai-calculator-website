337a5511dcab5fa5defba892c9300353f58fd71d
zachary107c <zacharynicholasm@gmail.com>
2024-10-19 12:11:19 +0530

    Final project

--- ai-calc/client/src/screens/home/index.tsx
+++ ai-calc/client/src/screens/home/index.tsx
@@ -1,7 +1,8 @@
 import { useEffect, useRef, useState } from "react";
 import { SWATCHES } from '@/constants'; 
 import { ColorSwatch , Group } from '@mantine/core';
-import {Button} from '@/components/ui/button';
+// import {Button} from '@/components/ui/button';
+import Draggable from 'react-draggable';
 import axios from 'axios';
 
 interface Response {
@@ -21,14 +22,33 @@ export default function Home() {
     const [color , setColor] = useState('rgb(255,255,255)');
     const [reset , setReset] = useState(false);
     const [result , setResult] = useState<GeneratedResult>();
+    const [latexExpression , setLatexExpression] = useState<Array<string>>([]);
+    const [latexPosition , setLatexPosition] = useState({x: 10 , y: 200}); // x and y position of the latex expression
     const [dictOfVars , setDictOfVars] = useState({});
 
     useEffect(() => {
         if(reset) {
             resetCanvas();
+            setLatexExpression([]);
+            setResult(undefined);
+            setDictOfVars({});
             setReset(false);
         }
     } , [reset]);
+
+    useEffect(() => {
+        if (result) {
+            renderLatexToCanvas(result.expression,result.answer);
+        }
+    } , [result]);
+
+    useEffect(() => {
+        if (latexExpression.length > 0 && window.MathJax) {
+            setTimeout(() => {
+                window.MathJax.Hub.Queue(["Typeset",window.MathJax.Hub])
+            },0);
+        }
+    } , [latexExpression])
 
     useEffect(() => {
         const canvas = canvasRef.current;
@@ -43,7 +63,35 @@ export default function Home() {
                 ctx.lineWidth = 3;
             }
         }
+
+        const script  = document.createElement('script');
+        script.src = 'https://cdnjs.cloudflare.com/ajax/libs/mathjax/2.7.9/config/TeX-MML-AM_CHTML.js'
+        script.async = true;
+        document.head.appendChild(script);
+
+        script.onload = () => {
+            window.MathJax.Hub.Config({
+                tex2jax: {inlineMath: [['$', '$'], ['\\(', '\\)']]}
+            })
+        };
+
+        return () => {
+            document.head.removeChild(script);
+        }
     } ,[] );
+
+    const renderLatexToCanvas = (expression: string , answer: string) => {
+        const latex = `\\(\\LARGE{${expression} = ${answer}}\\)`;
+        setLatexExpression([...latexExpression,latex]);
+
+        const canvas = canvasRef.current;
+        if (canvas) {
+            const ctx = canvas.getContext('2d');
+            if(ctx) {
+                ctx.clearRect(0,0,canvas.width,canvas.height);
+            }
+        }
+    };
 
     const sendData = async () => {
         const canvas = canvasRef.current;
@@ -59,9 +107,48 @@ export default function Home() {
             });
 
             const resp = await response.data;
-            console.log('Response: ',resp);
+            resp.forEach((data: Response) => {
+                if (data.assign == true) {
+                    setDictOfVars({
+                        ...dictOfVars,
+                        [data.expr] : data.result
+                    })
+                }
+            })
+            
+            const ctx = canvas.getContext('2d');
+            const imageData = ctx!.getImageData(0,0,canvas.width,canvas.height);
+            let minX = canvas.width , minY = canvas.height , maxX = 0 , maxY = 0;
+            
+            for(let y = 0 ; y < canvas.height ; y++){
+                for(let x = 0 ; x < canvas.width ; x++){
+                    if (imageData.data[(y * canvas.width + x) * 4 + 3] > 0) {
+                        if (x < minX) minX = x;
+                        if (x > maxX) maxX = x;
+                        if (y < minY) minY = y;
+                        if (y > maxY) maxY = y;
+                    }
+                }  
+            }
+
+            const centerX = (minX - maxX) / 2;
+            const centerY = (minY - maxY) / 2;
+
+            setLatexPosition({
+                x: centerX,
+                y: centerY
+            });
+
+            resp.data.forEach((data: Response) => {
+                setTimeout(() => {
+                    setResult({
+                        expression: data.expr,
+                        answer: data.result
+                    });
+                });
+            },200);
         }
-    }
+    };
 
     const resetCanvas = () => {
         const canvas = canvasRef.current;
@@ -145,6 +232,20 @@ export default function Home() {
                 onMouseUp={stopDrawing}
                 onMouseOut={stopDrawing}
             />
+
+            {latexExpression && latexExpression.map((latex , index) => {
+                <Draggable
+                    key={index}
+                    defaultPosition={latexPosition}
+                    onStop={(e,data) => setLatexPosition({x: data.x , y: data.y})}
+                >
+                    <div
+                        className="absolute text-white"
+                    >
+                        <div className="latex-content">{latex}</div>
+                    </div>
+                </Draggable>
+            })}
         </>
     );
 }
--- ai-calc/server/constants.py
+++ ai-calc/server/constants.py
@@ -1,3 +1,9 @@
-SERVER_URL = "localhost"
-PORT  = "8900"
-ENV = "dev"
+from dotenv import load_dotenv
+import os
+load_dotenv()
+
+SERVER_URL = 'localhost'
+PORT = '8900'
+ENV = 'dev'
+
+GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
